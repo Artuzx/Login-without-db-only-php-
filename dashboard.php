515100eb185c3d9dc91d2bcedfbcb52c4204f734
Artuzx <111784952+Artuzx@users.noreply.github.com>
2025-12-01 11:41:56 +0100

    Fixed now working

--- dashboard.php
+++ dashboard.php
@@ -1,19 +1,15 @@
 <?php
+
     session_start();
-    if (!isset($_SESSION["credenziali"])) {
-        $_SESSION["Nome"] = $_POST("Nome");
-        $_SESSION["Password"] = $_POST("Password");
+    if (!isset($_SESSION["Password"])) {
+        echo "ciao".$_POST["Nome"];
+        $_SESSION["Nome"] = $_POST["Nome"];
+        $_SESSION["Password"] = $_POST["Password"];
     }
-    
-    if ($_SESSION["Nome"] == "admin"){
-        if($_SESSION["Password"] == "admin"){
-            ?> 
-            <a href = "https://www.youtube.com/watch?v=xvFZjo5PgG0&list=RDxvFZjo5PgG0&start_radio=1&pp=ygUIcmlja3JvbGygBwE%3D"> Benvenuto! </a>
-            <?php
-        } else {
-            ?>
-            <h1> Devi crearti un account sul sito </h1>
-            <?php
-        }
+    var_dump($_SESSION);
+    if ($_SESSION["Nome"] == "admin" && $_SESSION["Password"] == "admin") {
+        echo '<a href="https://www.youtube.com/watch?v=xvFZjo5PgG0&list=RDxvFZjo5PgG0&start_radio=1&pp=ygUIcmlja3JvbGygBwE%3D">Benvenuto!</a>';
+    } else {
+        echo "<h1>Devi crearti un account sul sito</h1>";
     }
     ?>
